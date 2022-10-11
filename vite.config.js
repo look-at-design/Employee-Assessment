870f52237b483c948ce983b8b708aa136fcd5650
krishnamoorthy <ramalingampanchanatham@Ramalingams-Mac-mini.local>
2022-10-11 14:44:43 +0530

    git deployment -

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Employee-Assessment/',
   plugins: [vue()],
   resolve: {
     alias: {
