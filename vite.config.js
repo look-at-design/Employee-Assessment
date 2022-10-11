5420ccb812d42b8723d14461a1d604bc33924536
krishnamoorthy <ramalingampanchanatham@Ramalingams-Mac-mini.local>
2022-10-11 12:50:27 +0530

    adding dist path ~

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Employee-Assessment/',
   plugins: [vue()],
   resolve: {
     alias: {
